a553cd7004a3c02c0db1c54221a5f578335a2da5
NEULiee <asdfz888@naver.com>
2021-12-22 23:34:34 +0900

    3048 개미 문자열

---
+++ src/com/neuli/boj/boj_3048.java
@@ -0,0 +1,23 @@
+package com.neuli.boj;
+
+import java.util.Scanner;
+
+public class boj_3048 {
+
+    static int n1, n2;
+    static StringBuffer s1, s2, s;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+
+        s1 = new StringBuffer(sc.next());
+        s2 = new StringBuffer(sc.next());
+
+        s.append(s1);
+
+
+    }
+}
